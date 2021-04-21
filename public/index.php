602c56c5f8afbc2c37c51ea3f3200fdd3b9ed765
haterphp <norov.vladislav@yandex.ru>
2021-04-21 18:16:32 +0700

    Show Errors and Edit .htaccess

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require_once '../vendor/autoload.php';
 require_once '../core/helpers.php';
 
